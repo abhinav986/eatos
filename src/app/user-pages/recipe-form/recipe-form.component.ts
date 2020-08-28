5e8e6d0bbe437d61b3c6e061be4b99175e17bca6
kumara295 <Abhinav.Kumar2@emc.com>
2020-08-28 15:27:30 +0530

    recipe

--- src/app/user-pages/recipe-form/recipe-form.component.ts
+++ src/app/user-pages/recipe-form/recipe-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-recipe-form',
@@ -6,7 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-form.component.scss']
 })
 export class RecipeFormComponent implements OnInit {
+  toppings = new FormControl();
+  toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
+  
+  formGroup = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required),
+    confirmPassword: new FormControl('', Validators.required),
+    agreeToTerms: new FormControl(false, Validators.required),
+  });
 
+  slideConfig2 = {
+    className: 'center',
+    centerMode: true,
+    infinite: true,
+    centerPadding: '0',
+    slidesToShow: 1,
+    speed: 500,
+    dots: false,
+  };
   constructor() { }
 
   ngOnInit() {
